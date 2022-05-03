cb84a069d1ef0525ec4cc80e020ade6df89b942d
Nicole Barkley <barkleylikecharles@icloud.com>
2022-05-03 11:23:40 -0400

    update comment seeds content

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,169 +2,169 @@ const { Comment } = require('../models');
 
 const commentdata = [
   {
-    comment_text: 'Nunc rhoncus dui vel sem.',
+    comment_text: 'This looks great.',
     user_id: 6,
     post_id: 1
   },
   {
-    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    comment_text: 'Do not buy this.',
     user_id: 6,
     post_id: 8
   },
   {
-    comment_text: 'Aliquam erat volutpat. In congue.',
+    comment_text: 'This looks great.',
     user_id: 3,
     post_id: 8
   },
   {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    comment_text: 'This looks great.',
     user_id: 3,
     post_id: 8
   },
   {
-    comment_text: 'In hac habitasse platea dictumst.',
+    comment_text: 'Ugly.',
     user_id: 7,
     post_id: 6
   },
   {
-    comment_text: 'Vivamus vestibulum sagittis sapien.',
+    comment_text: 'This looks great.',
     user_id: 1,
     post_id: 3
   },
   {
-    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    comment_text: 'This looks great.',
     user_id: 6,
     post_id: 7
   },
   {
-    comment_text: 'Sed vel enim sit amet nunc viverra dapibus.',
+    comment_text: 'This looks great.',
     user_id: 7,
     post_id: 4
   },
   {
-    comment_text: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    comment_text: 'DO NOT purchase this!',
     user_id: 6,
     post_id: 8
   },
   {
-    comment_text: 'Morbi a ipsum.',
+    comment_text: 'Buy now',
     user_id: 6,
     post_id: 3
   },
   {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    comment_text: 'This looks great!',
     user_id: 3,
     post_id: 4
   },
   {
-    comment_text: 'Donec ut mauris eget massa tempor convallis.',
+    comment_text: 'Buy now',
     user_id: 5,
     post_id: 4
   },
   {
     comment_text:
-      'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+      'DO NOT purchase this!',
     user_id: 4,
     post_id: 9
   },
   {
     comment_text:
-      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
+      'Buy now',
     user_id: 5,
     post_id: 1
   },
   {
-    comment_text: 'Quisque porta volutpat erat.',
+    comment_text: 'Great product',
     user_id: 6,
     post_id: 2
   },
   {
-    comment_text: 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+    comment_text: 'Perfect gift.',
     user_id: 8,
     post_id: 2
   },
   {
     comment_text:
-      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+      'DO NOT purchase this!',
     user_id: 2,
     post_id: 4
   },
   {
-    comment_text: 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',
+    comment_text: 'Great product.',
     user_id: 4,
     post_id: 5
   },
   {
     comment_text:
-      'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+      'This is great.',
     user_id: 5,
     post_id: 6
   },
   {
     comment_text:
-      'Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
+      'So glad I purchased.',
     user_id: 9,
     post_id: 1
   },
   {
-    comment_text: 'Curabitur convallis.',
+    comment_text: 'DO NOT purchase this!',
     user_id: 6,
     post_id: 4
   },
   {
-    comment_text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    comment_text: 'This is great.',
     user_id: 4,
     post_id: 9
   },
   {
-    comment_text: 'Morbi non quam nec dui luctus rutrum.',
+    comment_text: 'Agreed',
     user_id: 3,
     post_id: 8
   },
   {
     comment_text:
-      'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.',
+      'DO NOT purchase this!',
     user_id: 8,
     post_id: 9
   },
   {
     comment_text:
-      'Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.',
+      'DO NOT purchase this!',
     user_id: 1,
     post_id: 2
   },
   {
-    comment_text: 'Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend.',
+    comment_text: 'Agreed.',
     user_id: 5,
     post_id: 3
   },
   {
-    comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    comment_text: 'Buy this now.',
     user_id: 1,
     post_id: 6
   },
   {
-    comment_text: 'Nam tristique tortor eu pede.',
+    comment_text: 'This is great.',
     user_id: 4,
     post_id: 6
   },
   {
-    comment_text: 'Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+    comment_text: 'This is great.',
     user_id: 4,
     post_id: 5
   },
   {
-    comment_text: 'Proin eu mi. Nulla ac enim.',
+    comment_text: 'DO NOT purchase this!',
     user_id: 4,
     post_id: 8
   },
   {
-    comment_text: 'Sed ante. Vivamus tortor.',
+    comment_text: 'DO NOT purchase this!',
     user_id: 7,
     post_id: 5
   },
   {
-    comment_text: 'Aliquam quis turpis eget elit sodales scelerisque.',
+    comment_text: 'This is a great purchase.',
     user_id: 8,
     post_id: 1
   }
