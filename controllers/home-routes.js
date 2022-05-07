b7f3498b63e33a880e5c62e0cd95aed9da74a90e
Your Name <kalaitzidispaul@gmail.com>
2022-05-07 14:14:22 -0400

    fixed styling and put request

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -61,6 +61,57 @@ router.post(
   }
 )
 
+router.put(
+  "/upload",
+  upload.single("image"),
+  (req,res) => {
+    // console.log(req.body)
+    const tempPath = req.file.path;
+    // console.log(tempPath)
+    const targetPath = path.join(__dirname, `../public/images/${req.file.originalname}`)
+    // console.log(req.file.originalname)
+    if(path.extname(req.file.originalname).toLowerCase() === ".png" || ".jpeg" || ".gif" ){
+      // fs.rename(tempPath, targetPath, err => {
+      //   console.log(targetPath)
+      //   if(err) return handleError(err, res)
+      // })
+      console.log(req.body)
+      console.log(req.file.originalname)
+      
+      console.log({
+        title: req.body["post-title"],
+        post_text: req.body['post-text'],
+        user_id: req.session.user_id,
+        image_name: req.file.originalname
+      })
+      Post.update({
+        title: req.body["post-title"],
+        post_text: req.body['post-text'],
+        user_id: req.session.user_id,
+        image_name: req.file.originalname
+  
+      })
+        .then(dbPostData => 
+          res.status(200)
+          .contentType("text/plain")
+          .redirect("/"))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        })
+    }else{
+      fs.unlink(tempPath, err=> {
+        if(err) throw(err)
+
+        res
+        .status(404)
+        .contentType("text/plain"
+        .end("only .png files are allowed!"))
+      })
+    }
+  }
+)
+
 // router.get("/image/:image", (req,res) => {
 //   // findAll(req.params.)
 
